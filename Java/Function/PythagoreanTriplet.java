216f2f27f68949b34dee3edc064debead68f27ca
Shumaila <sayedshumaila27@gmail.com>
2022-12-29 12:53:27 +0530

    Update PythagoreanTriplet.java

--- Java/Function/PythagoreanTriplet.java
+++ Java/Function/PythagoreanTriplet.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+//Write a function to check if a given triplet is a Pythagorean triplet or not. 
+//(A Pythagorean triplet is when the sum of the square of two numbers is equal to the square of the third number)
+
 public class PythagoreanTriplet {
     public static void main(String[] args) {
           pythagorean();
